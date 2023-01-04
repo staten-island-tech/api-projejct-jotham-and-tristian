244f2ef6b5e57059dfb8c0f9994ba5e9df47d34c
justthatonedude <113192774+justthatonedude@users.noreply.github.com>
2023-01-04 10:54:37 -0500

    Full Layout

--- app/virusapi/test.py
+++ app/virusapi/test.py
@@ -4,20 +4,10 @@ form = cgi.FieldStorage()
 userURL = form.getvalue("UserURL")
 userfile = form.getvalue("userFile")
 
-def FileChecker():
-    url = "https://www.virustotal.com/api/v3/files"
-    files = {"file": (userfile, open(userfile, "rb"), "image/jpeg")}
-    headers = {
-    "accept": "application/json",
-    "x-apikey": "29909ddf2acb1233e0cab2142ec6ea733e786e4d97ea4b631e70860acf0c61c6"
-    }
-    
-    response = requests.post(url, files=files, headers=headers)
-
-    print(response.text)
 
 
-def URLChecker():
+
+def URLChecker(userURL):
     url = "https://www.virustotal.com/api/v3/urls"
 
     payload = userURL
@@ -31,4 +21,5 @@ def URLChecker():
 
     print(response.text)
 
+URLChecker(userURL)
 
